65eed102200edff3df9a661d0c21f1320b294a43
Nymul Islam Moon <nymulislamlee@gmail.com>
2024-09-09 16:06:04 +0600

    app.php updated
    providers updated

--- config/app.php
+++ config/app.php
@@ -168,6 +168,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /**
+         * JWTAuthServiceProvider
+         */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ])->toArray(),
 
     /*
